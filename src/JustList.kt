0748ef8139d5c975e5bf673125c2cea718d120f2
vhontar <vladislav.hontar@gmail.com>
2024-08-24 00:10:04 +0200

    init

---
+++ src/JustList.kt
@@ -0,0 +1,123 @@
+sealed class JustList<out T> {
+    companion object {
+        fun <T> of(vararg aa: T): JustList<T> {
+            val tail = aa.sliceArray(1 until aa.size)
+            return if (aa.isEmpty()) Nil else Construct(aa[0], of(*tail))
+        }
+
+        fun <T> empty(): JustList<T> = Nil
+    }
+}
+
+data object Nil : JustList<Nothing>()
+data class Construct<out T>(val head: T, val tail: JustList<T>) : JustList<T>()
+
+fun <T> JustList<T>.tail(): JustList<T> = when (this) {
+    Nil -> throw IllegalArgumentException("Nil cannot have a tail")
+    is Construct -> tail
+}
+
+fun <T> JustList<T>.setHead(head: T): JustList<T> = when (this) {
+    Nil -> throw IllegalArgumentException("Cannot replace head of the Nil")
+    is Construct -> Construct(head, tail)
+}
+
+fun <T> JustList<T>.setTail(tail: JustList<T>): JustList<T> = when (this) {
+    Nil -> throw IllegalArgumentException("Cannot replace tail of the Nil")
+    is Construct -> Construct(head, tail)
+}
+
+tailrec fun <T> JustList<T>.drop(n: Int): JustList<T> {
+    if (n == 0) return this
+
+    return when (this) {
+        Nil -> throw IllegalArgumentException("Cannot drop more elements than in this JustList")
+        is Construct -> tail.drop(n - 1)
+    }
+}
+
+tailrec fun <T> JustList<T>.dropWhile(f: (T) -> Boolean): JustList<T> {
+    return when (this) {
+        Nil -> this
+        is Construct -> if (f(head)) tail.dropWhile(f) else this
+    }
+}
+
+fun <T> JustList<T>.dropLast(): JustList<T> = when (this) {
+    Nil -> throw IllegalArgumentException("Cannot initialize Nil")
+    is Construct -> if (tail == Nil) Nil else Construct(head, tail.dropLast())
+}
+
+fun <T> JustList<T>.append(other: JustList<T>): JustList<T> = when (this) {
+    Nil -> other
+    is Construct -> Construct(head, tail.append(other))
+}
+
+fun <T> JustList<T>.append2(other: JustList<T>) = foldRight(other) { head, tail -> Construct(head, tail) }
+
+fun <T> JustList<T>.append3(other: JustList<T>) = reversed().foldLeft(other) { tail, head -> Construct(head, tail) }
+
+
+fun <A, B> JustList<A>.foldRight(z: B, f: (A, B) -> B): B = when (this) {
+    is Construct -> f(head, tail.foldRight(z, f))
+    Nil -> z
+}
+
+tailrec fun <A, B> JustList<A>.foldLeft(z: B, f: (B, A) -> B): B = when (this) {
+    is Construct -> tail.foldLeft(f(z, head), f)
+    Nil -> z
+}
+
+// TODO Try to investigate more here!!
+fun <A, B> JustList<A>.foldRightL(z: B, f: (A, B) -> B): B =
+    foldLeft(
+        z = { tail: B -> tail },
+        f = { lambda, head ->
+            { tail: B -> lambda(f(head, tail)) }
+        }
+    )(z)
+
+fun <T> JustList<T>.length2(): Int = foldRight(0) { _, acc -> acc + 1 }
+
+fun <T> JustList<T>.length(): Int = foldLeft(0) { acc, _ -> acc + 1 }
+
+fun <T> JustList<T>.reversed(): JustList<T> = when (this) {
+    is Construct -> foldLeft(JustList.empty()) { tail, head -> Construct(head, tail) }
+    Nil -> this
+}
+
+fun <T> JustList<JustList<T>>.concat(): JustList<T> =
+    foldRightL(JustList.empty()) { list1, list2 -> list1.append(list2) }
+
+fun <A, B> JustList<A>.map(f: (A) -> B): JustList<B> =
+    foldRightL(JustList.empty()) { head, tail -> Construct(f(head), tail) }
+
+fun <A, B> JustList<A>.flatMap(f: (A) -> JustList<B>): JustList<B> =
+    foldRightL(JustList.empty()) { head, tail -> f(head).append(tail) }
+
+fun <T> JustList<T>.filter2(predicate: (T) -> Boolean): JustList<T> =
+    foldRightL(JustList.empty()) { head, tail -> if (predicate(head)) Construct(head, tail) else tail }
+
+fun <T> JustList<T>.filter(predicate: (T) -> Boolean): JustList<T> =
+    flatMap { head -> if (predicate(head)) JustList.of(head) else Nil }
+
+fun <A, B> JustList<A>.zipWith(other: JustList<A>, f: (A, A) -> B): JustList<B> =
+    when(this) {
+        Nil -> JustList.empty()
+        is Construct -> when(other) {
+            Nil -> JustList.empty()
+            is Construct -> Construct(f(head, other.head), tail.zipWith(other.tail, f))
+        }
+    }
+
+tailrec fun <T> JustList<T>.hasSubsequence(other: JustList<T>): Boolean = when(this) {
+    Nil -> false
+    is Construct -> when(other) {
+        Nil -> true
+        is Construct -> if (head == other.head) {
+            tail.hasSubsequence(other.tail)
+        } else {
+            tail.hasSubsequence(other)
+        }
+    }
+}
---
+++ src/JustTree.kt
@@ -0,0 +1,55 @@
+sealed class JustTree<out T>
+
+data class Leaf<T>(val value: T) : JustTree<T>() {
+    companion object {
+        fun <T> create(value: T): JustTree<T> = Leaf(value)
+    }
+}
+
+data class Branch<T>(val left: JustTree<T>, val right: JustTree<T>) : JustTree<T>()
+
+fun <T> JustTree<T>.size(): Int = when (this) {
+    is Leaf -> 1
+    is Branch -> 1 + left.size() + right.size()
+}
+
+fun <T> JustTree<T>.max(f: (T, T) -> T): T = when (this) {
+    is Leaf -> value
+    is Branch -> f(left.max(f), right.max(f))
+}
+
+fun <T> JustTree<T>.depth(): Int = when (this) {
+    is Leaf -> 0
+    is Branch -> 1 + maxOf(left.depth(), right.depth())
+}
+
+fun <A, B> JustTree<A>.map(f: (A) -> B): JustTree<B> = when (this) {
+    is Leaf -> Leaf(f(value))
+    is Branch -> Branch(left.map(f), right.map(f))
+}
+
+fun <A, B> JustTree<A>.fold(leaf: (A) -> B, branch: (B, B) -> B): B = when (this) {
+    is Leaf -> leaf(value)
+    is Branch -> branch(left.fold(leaf, branch), right.fold(leaf, branch))
+}
+
+fun <T> JustTree<T>.sizeF(): Int = fold(
+    leaf = { 1 },
+    branch = { a, b -> 1 + a + b },
+)
+
+fun <T> JustTree<T>.depthF(): Int = fold(
+    leaf = { 0 },
+    branch = { a, b -> 1 + maxOf(a, b) },
+)
+
+fun <T> JustTree<T>.maxF(f: (T, T) -> T): T = fold(
+    leaf = { it },
+    branch = { a, b -> f(a, b) },
+)
+
+fun <A, B> JustTree<A>.mapF(f: (A) -> B): JustTree<B> = fold(
+    leaf = { a -> Leaf.create(f(a)) },
+    branch = { a, b -> Branch(a, b) },
+)
+
